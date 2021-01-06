46d4ebc0bce841e3bd15dc7f316d399e733675db
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2021-01-06 11:52:05 +0900

    Add callback to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
 	validates :price, numericality: { greater_than: 0 }
 	validates :name, presence: true
+
+	after_initialize { p 'initialize' } # Item.new Item.first , колбек будет вызван после инициализации
+	after_save { p 'save' } # колбек будет вызван после Item.create либо после Item.save
+	#after_create { WhatsApp.sent_messege(self) } # колбек будет вызван после Item.create
+	after_update { p 'update' } # колбек будет вызван после Item.update
+	after_destroy { p 'destroy' } # колбек будет вызван после Item.last.destroy 
 end
