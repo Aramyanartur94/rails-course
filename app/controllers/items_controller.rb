eab20bbc78aac6e88a970b8caaed82e6b2b92ef5
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2021-01-06 13:28:13 +0900

    Params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,26 @@
 class ItemsController < ApplicationController
 	
+	layout false # не будет требовать view
+	skip_before_action :verify_authenticity_token	
+
 	def index
 		@items = Item.all
 		#render body: @items.map { |i| "#{i.name}: #{i.price}" }
 	end
 
+	def create
+		item = Item.create(items_params)
+		if item.persistet?
+			render json: item.name, status: :created
+		else
+			render json: item.errors, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def items_params
+		params.permit (*filters :name, :price)
+	end
 
 end
