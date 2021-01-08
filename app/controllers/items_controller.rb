e6fd0753463a571983535060f10c7da7727f4d35
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2021-01-08 21:32:50 +0900

    Add create, destroy, show methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,26 +1,54 @@
 class ItemsController < ApplicationController
-	
-	layout false # не будет требовать view
-	skip_before_action :verify_authenticity_token	
+		
+	skip_before_action :verify_authenticity_token
 
 	def index
 		@items = Item.all
-		#render body: @items.map { |i| "#{i.name}: #{i.price}" }
 	end
 
 	def create
-		item = Item.create(items_params)
-		if item.persistet?
-			render json: item.name, status: :created
+
+		item = Item.new(items_params)
+		if item.valid?
+			item.save
+			render plain: params[:item].inspect
+			#render json: item.name, status: :created
+		else
+			render body: "NO"
+			#render json: item.errors, status: :unprocessable_entity
+		end
+	end
+
+	
+	def new
+	end
+
+	def show
+		unless (@item = Item.where(id: params[:id]).first)
+			render body: 'Page not found', status: 4040
+		end
+	end
+
+	def edit
+	end
+
+	def update 
+		
+	end
+
+	def destroy
+		item = Item.where(id: params[:id]).first.destroy
+		if item.destroyed?
+			redirect_to items_path
 		else
 			render json: item.errors, status: :unprocessable_entity
 		end
 	end
 
 	private
-
+	
 	def items_params
-		params.permit (*filters :name, :price)
+		params.require(:item).permit(:name, :price)
 	end
-
+	
 end
