63e7037441bd47d44b150503a6de9048df56a419
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2021-02-13 21:45:29 +0900

    ActiveRecord

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,31 @@
 class ItemsController < ApplicationController
-		
+		layout "application"
 	skip_before_action :verify_authenticity_token
 	before_action :find_item, only: %i[show edit update destroy upvote]
 	before_action :admin?, only: %i[edit]
 	after_action :show_info, only: %i[index] 
 	
 	def index
-		@items = Item.all
+		#@items = Item.all.order(:votes_count)
+		#@items = Item.all.order(:price)
+		#@items = Item.all.sort
+		#@items = Item.all.order('votes_count DESC')
+		#@items = Item.all.order('votes_count DESC', 'price') сначала отсортировал по order_count потом отсортировал еще по price
+		#@items = Item.all.order('votes_count DESC', 'price').limit 3  (ограничело выборку 3 товара)
+		#@items = Item.all.order(:id).limit 10
+
+		#@items = Item.where('price: 200')
+		#@items = Item.where('votes_count: 3')
+		#@items = Item.where('price: 200, votes_count: 3')
+		#@items = Item.where('name: car, price: 200, votes_count: 3')
+		#@items = Item.where('price >= 200 OR votes_count >= 3')
+		#@items = Item.where('price >= ?', params[:price_from])
+
+		@items = Item
+		@items = @items.where('price >= ?', params[:price_from]) if params[:price_from]
+		@items = @items.where('created_at >= ?', 1.day.ago) if params[:today]
+		@items = @items.where('votes_count >= ?', params[:votes_from]) if params[:votes_from]
+		@items = @items.order(:id)
 	end
 
 	def create
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
 	end
 
 	root to: 'items#index'
+	
 end
